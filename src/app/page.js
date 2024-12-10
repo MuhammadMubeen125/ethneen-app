15d5074703ed630542ffba138f99f843487a90c0
Mubeen <mubeensoomro2002@gmail.com>
2024-12-10 07:38:35 -0600

    updated page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Projectform } from "./components/Projectform";
+// import { Projectform } from "./components/Projectform";
 // import SamplePage from "./components/SamplePage";
 // import {DashboardPage} from "./components/DashboardPage";
 import { PageOne } from "./components/PageOne";
